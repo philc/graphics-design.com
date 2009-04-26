21eae78830557ea05866f8e8ea1761862afb5c42
Phil Crosby <phil.crosby@gmail.com>
2009-04-26 02:50:42 -0700

    Move assets for downloads into public/

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "rubygems"
 require "sinatra"
-
+require "models"
+# todo: add some php routes in here so downloads/fonts.php works.
 # I'm storing the title for each page in the page's file; I then pass that title to the surrounding
 # template. Parse out the line <% @title = "About" %> from each view file.
 unless defined? TITLES
@@ -17,6 +18,14 @@ end
 
 get "/" do
   render_with_title :index
+end
+
+get "/:section/?" do
+  render_with_title "#{params[:section]}/index".to_sym
+end
+
+get "/:section/:subsection" do
+  render_with_title "#{params[:section]}/#{params[:subsection]}".to_sym
 end
 
 def render_with_title(page)
