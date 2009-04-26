beb75037fc9e7fefb2b6e2c399c978d330c17fdc
Phil Crosby <phil.crosby@gmail.com>
2009-04-26 03:47:05 -0700

    Moving portfolio assets into public directory

--- portfolio/index.php
+++ portfolio/index.php
@@ -2,26 +2,23 @@
 
 <b><%= title %></b><br>
 <img src="/layout/divider.png"><br>
-Welcome to my online portfolio. These are projects done under contract for clients. If you'd like to see my personal work instead of work done for clients, visit the <a href="../creations/">creations</a> section, although it is outdated compared to my portfolio.
-<br><br>
-<b>Note:</b> The images in the portfolio section are NOT FREE and are not to be reused or duplicated in any way. They are for viewing only.
-<br><br>
+Welcome to my online portfolio. These are projects done under contract for clients. If you'd like to see my personal work instead of work done for clients, visit the <a href="/creations/">creations</a>. 
+
+<p>
+Note: The images in the portfolio section are not to be reused or duplicated in any way.
+</p>
 
 <table border=0 cellpadding=0 cellspacing=4>
-<tr><td><a href="websites.php"><img src="/layout/icons/portfolioWebsites.jpg" border=0></a></td>
+<tr><td><a href="/portfolio/websites"><img src="/layout/icons/portfolioWebsites.jpg" border=0></a></td>
 <td valign=top><font face=arial><b>Websites</b><br>
-These are completed layouts for clients, with the layouts designed to the client's specs.
+Complete webpage layouts for clients.
 </td></tr>
-<tr><td><a href="logos.php"><img src="/layout/icons/portfolioLogos.jpg" border=0></a></td>
+<tr><td><a href="/portfolio/logos"><img src="/layout/icons/portfolioLogos.jpg" border=0></a></td>
 <td valign=top><font face=arial><b>Logos</b><br>
-These are logos not included in a full website layout, but designed separately. To see logos that were part of website layouts, visit the <a href="websites.php">websites</a> portion of my portfolio.
+Logos not included in a full website layout, but designed separately.
 </td></tr>
-<tr><td><a href="interfaces.php"><img src="/layout/icons/portfolioInterfaces.jpg" border=0></a></td>
+<tr><td><a href="/portfolio/interfaces"><img src="/layout/icons/portfolioInterfaces.jpg" border=0></a></td>
 <td valign=top><font face=arial><b>Interfaces</b><br>
-Standalone interfaces not designed in conjuction with the rest of the website.
+Standalone interfaces.
 </td></tr>
 </table>
-
-
-
-<? require("../templates/footer.inc"); ?>
