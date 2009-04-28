047ab6e76abb2f61026d50719fc22ea604022c7b
Phil Crosby <phil.crosby@gmail.com>
2009-04-28 00:38:18 -0700

    Add redirects for freeImages and webDesign, which have change their urls

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require "rubygems"
 require "sinatra"
 require "models"
+
+# Redirects for older versions of this site.
+# It used to be written in php and some pages link to .php
+get("/freeImages/:action") { redirect301 request.url.gsub("freeImages", "freeimages") }
+get("/freeImages/?") { redirect301 request.url.gsub("freeImages", "freeimages") }
+
+get("/webDesign/:action") { redirect301 request.url.gsub("webDesign", "webdesign") }
+get("/webDesign/?") { redirect301 request.url.gsub("webDesign", "webdesign") }
+
+def redirect301(url)
+  header "Location" => url
+  status 301 # try with 303 as well
+end
+
 # todo: add some php routes in here so downloads/fonts.php works.
 # I'm storing the title for each page in the page's file; I then pass that title to the surrounding
 # template. Parse out the line <% @title = "About" %> from each view file.
