164c7638fa8093f5d75873cc77bc73cd901bec30
Phil Crosby <phil.crosby@gmail.com>
2009-04-28 23:36:51 -0700

    Include google analytics

---
+++ public/analytics.js
@@ -0,0 +1,28 @@
+/* 
+ * Load google analytics asynchronously.
+ * http://lyncd.com/2009/03/better-google-analytics-javascript/
+ */
+function loadGoogleAnalytics(acct) {
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www."),
+      pageTracker,
+      s;
+  s = document.createElement('script');
+  s.src = gaJsHost + 'google-analytics.com/ga.js';
+  s.type = 'text/javascript';
+  s.onloadDone = false;
+  function init () {
+    pageTracker = _gat._getTracker(acct);
+    pageTracker._trackPageview();
+  }
+  s.onload = function () {
+    s.onloadDone = true;
+    init();
+  };
+  s.onreadystatechange = function() {
+    if (('loaded' === s.readyState || 'complete' === s.readyState) && !s.onloadDone) {
+      s.onloadDone = true;
+      init();
+    }
+  };
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
