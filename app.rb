3f2e8bf029e3b8a0b2683d3f9181ba9f38195004
Phil Crosby <phil.crosby@gmail.com>
2012-11-26 22:48:44 -0800

    Update for cedar, ruby 19, newer datamapper

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-require "rubygems"
+require "bundler/setup"
+require "pathological"
 require "sinatra"
 require "models"
 
--- models.rb
+++ models.rb
@@ -3,6 +3,7 @@ require "dm-core"
 
 # Functions for counting, like Word.all.count
 require "dm-aggregates"
+require "dm-migrations"
 
 # DATABASE_URL is provided by heroku.
 DataMapper.setup(:default, ENV["DATABASE_URL"] ||
@@ -11,54 +12,54 @@ DataMapper.setup(:default, ENV["DATABASE_URL"] ||
 class Download
   include DataMapper::Resource
 
-  property :id, Integer, :nullable => false, :serial => true, :key => true
-  property :title, String, :nullable => false, :length => 30
-  property :path, String, :nullable => false, :length => 40
+  property :id, Serial, :key => true
+  property :title, String, :length => 30
+  property :path, String, :length => 40
   property :thumbnail, String, :length => 40
   property :author, String, :length => 30
   property :description, Text
   property :rating, Integer
   property :size, Integer
-  property :date, DateTime, :nullable => false
-  property :type, String, :nullable => false
+  property :date, DateTime
+  property :type, String
   property :subtype, String, :length => 30
 end
 
 class Portfolio
   include DataMapper::Resource
 
-  property :id, Integer, :nullable => false, :serial => true, :key => true
-  property :title, String, :nullable => false, :limit => 40
+  property :id, Serial, :key => true
+  property :title, String
   property :description, Text
-  property :date, DateTime, :nullable => false
-  property :url, String, :limit => 45
-  property :cache_url, String, :limit => 45, :field => "cacheUrl"
-  property :thumbnail, String, :limit => 45
-  property :type, String, :limit => 30
+  property :date, DateTime
+  property :url, String
+  property :cache_url, String, :field => "cacheUrl"
+  property :thumbnail, String
+  property :type, String
 end
 
 class Creation
   include DataMapper::Resource
 
-  property :id, Integer, :nullable => false, :serial => true, :key => true
-  property :title, String, :nullable => false, :limit => 30
-  property :path, String, :limit => 30
+  property :id, Serial, :key => true
+  property :title, String
+  property :path, String
   property :description, Text
-  property :date, DateTime, :nullable => false
-  property :type, String, :limit => 30
+  property :date, DateTime
+  property :type, String
 end
 
 
 class FreeImage
   include DataMapper::Resource
-  property :id, Integer, :nullable => false, :serial => true, :key => true
-  property :path, String, :limit => 30
+  property :id, Serial, :key => true
+  property :path, String
   property :title, Text
   property :description, Text
-  property :items_in_set, Integer, :nullable => false, :field => "itemsInSet"
+  property :items_in_set, Integer, :field => "itemsInSet"
   property :size, Float
-  property :date, DateTime, :nullable => false
-  property :type, String, :limit => 30, :nullable => false
+  property :date, DateTime
+  property :type, String
 
   def file_extension
     self.type == "texture" ? "jpg" : "png"
